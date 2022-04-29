2887ec2965ed00c47e3692610dbcd7f84e159e0d
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-04-29 08:25:00 +0200

    Initial commit

---
+++ src/main/java/com/sda/conferenceroomreservationservice/ConferenceRoomReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sda.conferenceroomreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomReservationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/conferenceroomreservationservice/ConferenceRoomReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.conferenceroomreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
