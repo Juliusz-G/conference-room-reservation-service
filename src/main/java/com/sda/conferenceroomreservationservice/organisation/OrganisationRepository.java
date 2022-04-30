167f3c6b8d435514c4c952c99287c870cb2647aa
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-04-30 18:45:07 +0200

    Add Organisation repository interface

---
+++ src/main/java/com/sda/conferenceroomreservationservice/organisation/OrganisationRepository.java
@@ -0,0 +1,8 @@
+package com.sda.conferenceroomreservationservice.organisation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+}
