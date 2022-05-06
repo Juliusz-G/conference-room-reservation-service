c298a333e8f0acfe544e932a1e4d929fd7f9aeb0
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-05-06 09:59:57 +0200

    Change validation and cross-origin fixed

--- src/main/java/com/sda/conferenceroomreservationservice/ConferenceRoomReservationServiceApplication.java
+++ src/main/java/com/sda/conferenceroomreservationservice/ConferenceRoomReservationServiceApplication.java
@@ -3,7 +3,6 @@ package com.sda.conferenceroomreservationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@CrossOrigin("http://localhost:4200") !!! for angular app !!!
 @SpringBootApplication
 public class ConferenceRoomReservationServiceApplication {
 
--- src/main/java/com/sda/conferenceroomreservationservice/organisation/Organisation.java
+++ src/main/java/com/sda/conferenceroomreservationservice/organisation/Organisation.java
@@ -1,14 +1,10 @@
 package com.sda.conferenceroomreservationservice.organisation;
 
-import com.sda.conferenceroomreservationservice.organisation.validation.UniqueName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -16,6 +12,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 public class Organisation {
 
     @Id
@@ -24,7 +21,7 @@ public class Organisation {
 
     @NotEmpty(message = "Organisation name can not be blank or empty")
     @Size(min = 2, max = 20, message = "Organisation name must be between 2 - 20 characters long")
-    @UniqueName(message = "Organisation name have to be unique")
+//    @UniqueName(message = "Organisation name have to be unique")
     private String name;
 
     @NotEmpty(message = "Organisation description can not be blank or empty")
