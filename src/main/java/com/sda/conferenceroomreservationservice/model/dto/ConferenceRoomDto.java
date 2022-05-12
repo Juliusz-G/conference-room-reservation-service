616ca9ba4cd650c859576223614cbb23c603dc4f
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-05-12 11:05:42 +0200

    feature(crrs): extend dto and mapper with new fields in ConferenceRoom model class

--- src/main/java/com/sda/conferenceroomreservationservice/model/dto/ConferenceRoomDto.java
+++ src/main/java/com/sda/conferenceroomreservationservice/model/dto/ConferenceRoomDto.java
@@ -1,9 +1,13 @@
 package com.sda.conferenceroomreservationservice.model.dto;
 
+import com.sda.conferenceroomreservationservice.model.entity.Organisation;
+import com.sda.conferenceroomreservationservice.model.entity.Reservation;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -15,5 +19,8 @@ public class ConferenceRoomDto {
     private final String identifier;
     private final Integer level;
     private final Boolean availability;
-    private final Integer numberOfPlaces;
+    private final Integer numberOfStandingPlaces;
+    private final Integer numberOfSittingPlaces;
+    private final List<Reservation> reservationList;
+    private final Organisation organisation;
 }
