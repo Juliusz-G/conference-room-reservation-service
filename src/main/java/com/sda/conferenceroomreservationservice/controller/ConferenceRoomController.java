95822c56ce81015326ba2184ef2cfefe365fd190
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-05-12 12:32:59 +0200

    fix(crrs): clear

--- src/main/java/com/sda/conferenceroomreservationservice/controller/ConferenceRoomController.java
+++ src/main/java/com/sda/conferenceroomreservationservice/controller/ConferenceRoomController.java
@@ -1,7 +1,8 @@
 package com.sda.conferenceroomreservationservice.controller;
 
+import com.sda.conferenceroomreservationservice.model.dto.ConferenceRoomDto;
 import com.sda.conferenceroomreservationservice.service.ConferenceRoomService;
-import com.sda.conferenceroomreservationservice.model.entity.ConferenceRoom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,46 +10,42 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-// Annotation in order to simplify the creation of RESTful web services.
-// It's a convenient annotation that combines @Controller and @ResponseBody,
-// which eliminates the need to annotate every request handling method of
-// the controller class with the @ResponseBody annotation
 @RestController
-// the class-level annotation maps a specific request path or pattern onto a controller
 @RequestMapping("/conference-room")
 public class ConferenceRoomController {
 
-    public ConferenceRoomController(ConferenceRoomService service) {
-        super(service);
+    private final ConferenceRoomService conferenceRoomService;
+
+    @Autowired
+    public ConferenceRoomController(ConferenceRoomService conferenceRoomService) {
+        this.conferenceRoomService = conferenceRoomService;
     }
 
-    public ResponseEntity<ConferenceRoom> create(ConferenceRoom entity) {
-        return super.create(entity);
-    }
-
-    public ResponseEntity<ConferenceRoom> remove(ConferenceRoom entity) {
-        super.remove(entity);
+    public ResponseEntity<ConferenceRoomDto> create() {
         return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<ConferenceRoom> removeById(Long id) {
-        super.removeById(id);
+    public ResponseEntity<ConferenceRoomDto> remove() {
         return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<ConferenceRoom> update(ConferenceRoom entity) {
-        return super.update(entity);
+    public ResponseEntity<ConferenceRoomDto> removeById() {
+        return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<Optional<ConferenceRoom>> getById(Long id) {
-        return super.getById(id);
+    public ResponseEntity<ConferenceRoomDto> update() {
+        return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<Optional<ConferenceRoom>> getByName(String name) {
-        return super.getByName(name);
+    public ResponseEntity<Optional<ConferenceRoomDto>> getById() {
+        return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<List<ConferenceRoom>> getAll() {
-        return super.getAll();
+    public ResponseEntity<Optional<ConferenceRoomDto>> getByName() {
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<List<ConferenceRoomDto>> getAll() {
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/sda/conferenceroomreservationservice/service/ReservationService.java
+++ src/main/java/com/sda/conferenceroomreservationservice/service/ReservationService.java
@@ -1,12 +1,6 @@
 package com.sda.conferenceroomreservationservice.service;
 
-import com.sda.conferenceroomreservationservice.model.entity.Reservation;
-import com.sda.conferenceroomreservationservice.repository.ReservationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ReservationService {
