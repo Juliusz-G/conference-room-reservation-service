f7a5219eb64afc42bc2099f466b74f662f3ccdde
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-05-17 15:01:18 +0200

    change(crrs): other entities got JPA more specific implementation

--- src/main/java/com/sda/conferenceroomreservationservice/model/entity/Organisation.java
+++ src/main/java/com/sda/conferenceroomreservationservice/model/entity/Organisation.java
@@ -5,28 +5,74 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.List;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "organisation")
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"name"})})
+@Table(
+        name = "organisation",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "organisation_name_unique",
+                        columnNames = "organisation_name"
+                )
+        }
+)
 public class Organisation {
 
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 20;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "organisation_sequence",
+            sequenceName = "organisation_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "organisation_sequence"
+    )
+    @Column(
+            name = "organisation_id",
+            updatable = false
+    )
     private Long id;
 
     @NotEmpty(message = "Organisation name can not be blank or empty")
-    @Size(min = 2, max = 20, message = "Organisation name must be between 2 - 20 characters long")
+    @Size(
+            min = MIN_NAME_LENGTH,
+            max = MAX_NAME_LENGTH,
+            message = "Organisation name must be between 2 - 20 characters long"
+    )
+    @Column(
+            name = "organisation_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String name;
 
     @NotEmpty(message = "Organisation description can not be blank or empty")
+    @Column(
+            name = "description",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String description;
 
     @OneToMany
--- src/main/java/com/sda/conferenceroomreservationservice/model/entity/Reservation.java
+++ src/main/java/com/sda/conferenceroomreservationservice/model/entity/Reservation.java
@@ -5,25 +5,54 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "reservation")
-@Table
+@Table(name = "reservation")
 public class Reservation {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "reservation_sequence",
+            sequenceName = "reservation_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "reservation_sequence"
+    )
+    @Column(
+            name = "reservation_id",
+            updatable = false
+    )
     private Long id;
 
-//    @NotEmpty(message = "Date and time of start reservation can not be blank or empty.")
+    @Column(
+            name = "start_date_time",
+            nullable = false,
+            columnDefinition = "TIMESTAMP"
+    )
     private LocalDateTime startDateTime;
 
-//    @NotEmpty(message = "Date and time of start reservation can not be blank or empty.")
+    @Column(
+            name = "end_date_time",
+            nullable = false,
+            columnDefinition = "TIMESTAMP"
+    )
     private LocalDateTime endDateTime;
 
     @ManyToOne(cascade = CascadeType.REMOVE)
